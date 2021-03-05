070ecdd6911a7f669096a28a83b48a224b13546c
kaaytsaak <kaaytsaak@gmail.com>
2021-03-04 20:01:57 -0600

    App usuario funcionando

---
+++ app/src/main/java/com/netsec/taxiplusdriver/ViajeUsuario.java
@@ -0,0 +1,13 @@
+package com.netsec.taxiplusdriver;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViajeUsuario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_viaje_usuario);
+    }
+}
